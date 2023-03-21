ec2fc91088432ff182b7fdeb9a715d704ec82747
ampatte <ampatterson1@hotmail.com>
2023-03-21 02:29:41 -0400

    changed name back to getDb from getAllDb

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -25,8 +25,8 @@ export const putDb = async (id, content) => {
 };
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getAllDb = async () => {  
-  console.error('getAllDb not implemented');
+export const getDb = async () => {  
+  console.error('getDb not implemented');
   console.log('GET all from the database');
   const jateDb = await openDB('jate', 1);
   const tx = jateDb.transaction('jate', 'readonly');
